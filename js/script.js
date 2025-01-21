e5f58ab16d4f7602378202ae25238203230176ee
Florencia Collosso <florcollosso@gmail.com>
2025-01-21 01:18:26 -0300

    Emailjs agregado, formulario funcional, footer actualizado

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ galleryImages.forEach((img) => {
 });
 
 // Abrir y cerrar modal
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const fqaItems = document.querySelectorAll('.fqa-item');
 
   // Abrir la primera pregunta por defecto
@@ -22,32 +22,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Añadir evento de click a cada pregunta
   fqaItems.forEach((item) => {
-      const question = item.querySelector('.fqa-question');
-      const answer = item.querySelector('.fqa-answer');
+    const question = item.querySelector('.fqa-question');
+    const answer = item.querySelector('.fqa-answer');
 
-      question.addEventListener('click', () => {
-          // Cerrar todas las preguntas abiertas
-          fqaItems.forEach((el) => {
-              const elAnswer = el.querySelector('.fqa-answer');
-              const elQuestion = el.querySelector('.fqa-question');
-              if (el !== item) {
-                  el.classList.remove('open');
-                  elAnswer.style.height = '0'; 
-                  elQuestion.classList.remove('open');
-              }
-          });
-
-          // Abrir la pregunta clickeada
-          if (item.classList.contains('open')) {
-              item.classList.remove('open');
-              answer.style.height = '0'; 
-              question.classList.remove('open');
-          } else {
-              item.classList.add('open');
-              answer.style.height = answer.scrollHeight + 'px';
-              question.classList.add('open'); 
-          }
+    question.addEventListener('click', () => {
+      // Cerrar todas las preguntas abiertas
+      fqaItems.forEach((el) => {
+        const elAnswer = el.querySelector('.fqa-answer');
+        const elQuestion = el.querySelector('.fqa-question');
+        if (el !== item) {
+          el.classList.remove('open');
+          elAnswer.style.height = '0';
+          elQuestion.classList.remove('open');
+        }
       });
+
+      // Abrir la pregunta clickeada
+      if (item.classList.contains('open')) {
+        item.classList.remove('open');
+        answer.style.height = '0';
+        question.classList.remove('open');
+      } else {
+        item.classList.add('open');
+        answer.style.height = answer.scrollHeight + 'px';
+        question.classList.add('open');
+      }
+    });
   });
 });
 
@@ -55,8 +55,91 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.getElementById('toggler-btn');
   const body = document.body;
-  
-  toggleButton.addEventListener('click', function() {
+
+  toggleButton.addEventListener('click', function () {
     body.classList.toggle('menu-open');
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Formulario de contacto
+  // Inicializa EmailJS
+  emailjs.init("SYptYp4MIJcT_6EE1");
+
+  const form = document.getElementById("contact-form");
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
+
+  // Validación del formulario
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const isValid = validateForm();
+
+    if (isValid) {
+      try {
+        const formData = {
+          name: nameInput.value,
+          email: emailInput.value,
+          message: messageInput.value,
+        };
+
+        await emailjs.send("service_c36ccz4", "plantilla_en78lyn", formData);
+        alert("¡Mensaje enviado exitosamente!");
+        form.reset();
+        clearErrors();
+      } catch (error) {
+        alert("Hubo un problema al enviar tu mensaje. Inténtalo más tarde.");
+        console.error("Error al enviar el mensaje:", error);
+      }
+    }
+  });
+
+   // Función para validar los campos del formulario
+   function validateForm() {
+    let isValid = true;
+    clearErrors();
+    
+    // Validar nombre
+    if (nameInput.value.trim() === "") {
+      showError(nameInput, "Por favor, ingresa tu nombre.");
+      isValid = false;
+    }
+
+    // Validar correo electrónico
+    if (emailInput.value.trim() === "") {
+      showError(emailInput, "Por favor, ingresa un correo electrónico válido.");
+      isValid = false;
+    }
+
+    // Validar mensaje
+    if (messageInput.value.trim().length < 10) {
+      showError(messageInput, "Por favor, ingresa una consulta de al menos 10 caracteres.");
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  // Función para mostrar errores directamente en el placeholder
+  function showError(input, message) {
+    input.placeholder = message; 
+    input.classList.add("input-error");
+  }
+
+  // Función para limpiar errores
+  function clearErrors() {
+    nameInput.placeholder = "Ingresa tu nombre completo";
+    emailInput.placeholder = "Ingresa tu correo electrónico";
+    messageInput.placeholder = "Ingresa tu consulta";
+
+    // Eliminar la clase input-error de todos los campos
+    nameInput.classList.remove("input-error");
+    emailInput.classList.remove("input-error");
+    messageInput.classList.remove("input-error");
+
+    messageInput.value = "";
+  }
+
 });
